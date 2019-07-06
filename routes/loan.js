ae4ae6f53352b3c96033986dd30afbc7722c3cc3
= <=>
2019-07-06 06:33:01 +0100

    Added the get-all-users-route

--- routes/loan.js
+++ routes/loan.js
@@ -57,4 +57,23 @@ router.post('/login', (req, res) => {
   res.status(200).send({ user });
 });
 
+//Should get all users
+// @route is /user/all
+router.get('/all', (req, res) => {
+  const users = currentUsers;
+  res.status(200).send({ users });
+});
+
+// This route should return a single user by id
+router.get('/:user_id', async (req, res) => {
+  try {
+    const user = await currentUsers.find(
+      user => user.id === Number(req.params.user_id)
+    );
+    res.status(200).send({ user });
+  } catch (e) {
+    res.status(500).send('server error');
+  }
+});
+
 module.exports = router;
