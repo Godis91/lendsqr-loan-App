5d45bad555f4bc7679d17ed49ef4b7575ea3e791
= <=>
2019-07-06 06:28:27 +0100

    Added the Loans data to the Model

---
+++ models/Loans.js
@@ -0,0 +1,22 @@
+const availableLoans = [
+  {
+    id: 1,
+    name: 'Ren Money',
+    description: 'Salary earners discounted loan',
+    interestRate: '3%',
+    amount: 50000,
+    tenure: '18 months'
+  },
+  {
+    id: 2,
+    name: 'Kia kia',
+    description: 'Easy small loan',
+    interestRate: '5%',
+    amount: 5000,
+    tenure: '3 months'
+  }
+];
+
+module.exports = {
+  availableLoans
+};
