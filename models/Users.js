c27e58f4ed012f6710631c5d84bb6b2345a2c37c
= <=>
2019-07-06 06:27:13 +0100

    added the User data on the model

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+const currentUsers = [
+  {
+    id: 1,
+    firstName: 'Michael',
+    lastName: 'James',
+    email: 'mike@yahoo.com',
+    password: 'test1234',
+    loans: []
+  },
+  {
+    id: 2,
+    firstName: 'Joshua',
+    lastName: 'Matthew',
+    email: 'josh@yahoo.com',
+    password: 'test1234',
+    loans: []
+  }
+];
+
+module.exports = {
+  currentUsers
+};
