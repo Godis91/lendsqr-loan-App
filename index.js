1a7b407d4298946fca4bc1970ec154c6a19ff24f
= <=>
2019-07-06 06:25:15 +0100

    express config on index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// set up express app
+const app = express();
+
+// set up middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set up routes
+app.use('/users', require('./routes/user'));
+app.use('/loans', require('./routes/loan'));
+
+const port = process.env.PORT || 5000;
+
+// listen to server
+app.listen(port, () => console.log(`server is up on port ${port}`));
